a962fb6d523a8734f9cbfbb6ca6be862397ebaa8
Ritu Bafna <ritujane78@gmail.com>
2026-06-24 19:16:53 +0545

    create templates for start screen and the actual translating page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-  import React from 'react'
-  import TranslatorStart from './components/TranslatorStart'
+import { useState } from 'react'
+import TranslatorStart from './Components/TranslatorStart';
+import TranslatorApp from './Components/TranslatorApp.jsx';
 
-  const App = () => {
-    return (
-      <div>
-        <TranslatorStart />
+
+const App = () => {
+
+  return (
+    <div className="w-full h-screen bg-gradient-to-l from-[#b6f492] to-[#338b93] flex justify-center items-center">
+      <div className="w-[90%] max-w-lg bg-[#2d2d2d] rounded-xl shadow-2xl shadow-gray-800 flex flex-col ">
+        <TranslatorApp />
       </div>
-    )
-  }
+    </div>
+  )
+}
 
-  export default App
+export default App
