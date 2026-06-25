48382fffe220a8364318246e49c6508dd247a4e1
Ritu Bafna <ritujane78@gmail.com>
2026-06-25 18:53:52 +0545

    make website responsive

--- src/components/TranslatorStart.jsx
+++ src/components/TranslatorStart.jsx
@@ -4,7 +4,7 @@ const TranslatorStart = ({ onStart }) => {
 
   return (
     <div className="w-full h-full flex flex-col justify-center items-center p-6 sm:p-12">
-      <div className="w-full h-64 bg-gradient-to-l from-[#b6f492] to-[#338b93] rounded-t-full rounded-bl-full flex flex-col justify-center text-gray-700 pr-6">
+      <div className="w-full h-64 bg-gradient-to-l from-[#b6f492] to-[#338b93] rounded-t-full rounded-bl-full flex flex-col justify-center text-gray-700 pr-6 sm:p-12">
         <span className="font-shojumaru text-5xl sm:text-6xl text-center">Hello</span>
         <span className="text-2xl sm:text-3xl text-center">გამარჯობა</span>
         <span className="font-noto-sans text-3xl sm:text-4xl text-right">こんにちは</span>
